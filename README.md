2d088007306772cadf4c3d6cc23290d8c3cee65f
Dong Li <lidong.main@gmail.com>
2018-03-04 16:01:40 -0800

    added hello world example with spring boot

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# try-spring-boot
+# Try Spring Boot
+
+## Build
+
+ `./gradlew build`
+
+## Unit Test
+
+ `./gradlew test`
+
+## Run
+
+ ```./gradlew bootRun```
+
+ or
+
+ ```java -jar build/libs/try-spring-boot-0.0.1-SNAPSHOT.jar```
+
+## Test
+
+ ```
+ curl -X GET http://localhost:8080/hello
+ ```
---
+++ src/main/java/com/example/tryspringboot/TrySpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.example.tryspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrySpringBootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TrySpringBootApplication.class, args);
+  }
+}
